beafa45586cace20c936fecb193fcd9c5ce6faef
Oyeleye Mayowa <oyeleyemayowa24@gmail.com>
2020-08-04 02:41:26 -0700

    Product Collection and Product shows

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return new ProductCollection(Product::all());
+        return  ProductCollection::collection(Product::all());
         //
     }
 
