c583d753826d66f761c8937f6d852f361e80a862
Oyeleye Mayowa <oyeleyemayowa24@gmail.com>
2020-08-05 13:51:34 -0700

    Review Resource and Product Pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Model\Product;
+
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
@@ -16,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return  ProductCollection::collection(Product::all());
+        return  ProductCollection::collection(Product::paginate(20));
         //
     }
 
