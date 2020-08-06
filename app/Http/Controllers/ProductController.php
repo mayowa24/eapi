343e3cac2b19f88d732b0f4e59dd7685a5dad32e
Oyeleye Mayowa <oyeleyemayowa24@gmail.com>
2020-08-06 12:21:52 -0700

    Product Destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -105,6 +105,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return "deleted successfully";
     }
 }
